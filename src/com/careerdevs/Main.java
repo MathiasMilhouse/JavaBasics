d26ca0bf4e341720a3dacff9ef4e07f772723f9f
MathiasMilhouse <mathiasmilhouse.student@careerdevs.com>
2022-01-06 20:42:37 -0500

    Java Basics 2

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -2,7 +2,17 @@ package com.careerdevs;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static String createFullName(String str1, String str2){
+        System.out.println(str1 + " " + str2);
+        return(str1 + " " + str2);
     }
+
+    public static void main(String[] args) {
+	String firstName = "Mathias";
+    String lastName = "Milhouse";
+    String fullName = firstName + " " + lastName;
+    System.out.println(fullName);
+    System.out.println(createFullName("Mathias", "Milhouse"));
+    }
+
 }
