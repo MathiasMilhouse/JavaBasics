239d1b41291c75bcd24039258ed3774e538a54bc
MathiasMilhouse <mathiasmilhouse.student@careerdevs.com>
2022-01-20 15:54:47 -0500

    sayHelloAB

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -1,10 +1,23 @@
 package com.careerdevs;
+import java.util.Scanner;
 
 public class Main {
 
     public static String createFullName(String str1, String str2){
         System.out.println(str1 + " " + str2);
         return(str1 + " " + str2);
+    }
+
+    public static void whatIsYourFullName(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first name:");
+        String fName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        String lName = scanner.nextLine();
+
+        System.out.println("It is so nice to formally meet you " + fName + " " + lName);
     }
 
     public static void main(String[] args) {
@@ -14,5 +27,4 @@ public class Main {
     System.out.println(fullName);
     System.out.println(createFullName("Mathias", "Milhouse"));
     }
-
 }
---
+++ src/com/careerdevs/sayhello/Main.java
@@ -0,0 +1,32 @@
+package com.careerdevs.sayhello;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void whatIsYourName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String name = scanner.nextLine();
+
+        System.out.println("It is so nice to formally meet you " + "" + name);
+    }
+
+    public static void whatIsYourFullName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first name:");
+        String fName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        String lName = scanner.nextLine();
+
+        System.out.println("It is so nice to formally meet you " + fName + " " + lName);
+    }
+
+    public static void main(String[] args) {
+        whatIsYourName();
+        whatIsYourFullName();
+    }
+}
