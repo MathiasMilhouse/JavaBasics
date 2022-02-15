653baa5b69698baef0236d100ef1151654fb9f88
MathiasMilhouse <mathiasmilhouse.student@careerdevs.com>
2022-02-15 17:32:10 -0500

    Object Factory

--- src/com/careerdevs/firetruckfactory/Firetruck.java
+++ src/com/careerdevs/firetruckfactory/Firetruck.java
@@ -1,4 +1,34 @@
 package com.careerdevs.firetruckfactory;
 
 class Class{
+
+    private String name;
+    private int age;
+    private int wheels;
+    private Boolean red;
+
+    public void Main(String name, int age, int wheels, boolean red){
+        this.name = name;
+        this.age = age;
+        this.wheels = wheels;
+        this.red = false;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getAge(){
+        return age + " " + "years old";
+    }
+
+    public String getWheels(){
+        return wheels + " " + " wheels";
+    }
+
+    public Boolean getRed(){
+        return red;
+    }
+
+
 }
